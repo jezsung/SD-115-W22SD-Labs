861bed3a33017af6a4d5d568d79548d52c37b331
Jaesung Hong <jezsung@gmail.com>
2022-07-06 22:38:09 -0500

    add features

---
+++ SD-115-W22SD-Labs/Controllers/RoomsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SD_115_W22SD_Labs.Models;
+using System.Diagnostics;
+
+namespace SD_115_W22SD_Labs.Controllers
+{
+    public class RoomsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Hotel.Rooms);
+        }
+        
+        public IActionResult TotalCapacity()
+        {
+            ViewBag.TotalNumberOfRoomRemaining = Hotel.Rooms.Aggregate(0, (prev, room) => prev + (room.Occupied ? 0 : 1));
+            return View(Hotel.Rooms);
+        }
+
+        [HttpGet]
+        [Route("Rooms/Occupants/{occupants:int}")]
+        public IActionResult Occupants(int occupants)
+        {
+            Room? room = Hotel.Rooms.OrderBy((room) => room.Number).FirstOrDefault((room) => room.Capacity >= occupants);
+            return View(room);
+        }
+    }
+}
